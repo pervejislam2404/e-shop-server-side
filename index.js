94b37ddab614714dcbac3c23e59a856e1166578e
pervejislam2404 <pervejislam2404@gmail.com>
2021-12-17 18:11:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,350 @@
+const ObjectId = require('mongodb').ObjectId;
+var admin = require("firebase-admin");
+const { MongoClient } = require('mongodb');
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const fileUpload = require('express-fileupload');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
+// jwt
+const serviceAccount = require("./baby-care-products-50b0d-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// jwt
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvyqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// jwt
+
+async function verifyToken(req,res,next){
+    if(req?.headers?.authorization?.startsWith('Bearer ')){
+        const token = req.headers?.authorization.split(' ')[1];   
+        try{
+           const decodedUser = await admin.auth().verifyIdToken(token);
+           req.decodedEmail= decodedUser.email;
+        }
+        catch{
+    
+        }
+        }
+        next()
+    }
+// jwt
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("e-shop");
+        const shopCollections = database.collection("shopProducts");
+        const userCollections = database.collection("users");
+        const myCollections = database.collection("myProducts");
+        const reviewCollections = database.collection("userReview");
+
+        app.get('/products/:category', async(req, res) => {
+            const category = req.params.category;
+            const query = { category: category }
+            const result = await shopCollections.find(query).toArray();
+            res.json(result);
+        })
+
+
+        app.get('/allProducts', async(req, res) => {
+           const cursor = shopCollections.find({});
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products;
+            const count = await cursor.count();
+
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+            res.send({
+                count,
+                products
+            });
+        })
+
+
+        app.get('/detailOne/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await shopCollections.findOne(query);
+            res.json(result);
+        })
+
+        app.post('/setUser', async(req, res) => {
+            const user = req.body;
+            const result = await userCollections.insertOne(user);
+            res.send(result)
+        })
+
+        app.put('/setUser', async(req, res) => {
+            const user = req.body;
+            const upsert = { upsert: true };
+            const filter = { email: user.email };
+            const updateDocs = {
+                $set: { email: user.email, name: user.name },
+            };
+            const result = await userCollections.updateOne(filter, updateDocs, upsert);
+            res.send(result)
+        })
+
+        app.get('/checkAdmin/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await userCollections.findOne(query);
+            if (result?.role === 'admin') {
+                res.json(true);
+            } else {
+                res.json(false);
+            }
+        })
+
+
+        app.post('/myProduct', async(req, res)=>{
+            const product= req.body;
+            const result = await myCollections.insertOne(product);
+            res.json(result);
+        })
+
+        // review
+        app.post('/saveReview', async (req,res)=>{
+            const name = req.body.name;
+            const email = req.body.email;
+            const ratting = req.body.ratting;
+            const description = req.body.description;
+            const pic = req.files.img;
+            const picData = pic?.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const doctor = {
+                description,
+                ratting,
+                name,
+                email,
+                img: imageBuffer
+            }
+            const result = await reviewCollections.insertOne(doctor);
+            res.json(result);
+        })
+
+        //all-review
+
+        app.get('/getAllReview', async (req,res)=>{
+            const query = {};
+            const result = await reviewCollections.find(query).toArray();
+            res.json(result);
+        })
+
+
+        app.get('/getAllOrders',verifyToken, async (req,res)=>{
+            const query = {};
+            if(req?.decodedEmail){
+                const result = await myCollections.find(query).toArray();
+                res.json(result);
+            }else{
+                res.json([])
+            }
+        })
+
+
+          // delete-an-order-and-verify-user-with-jwt
+          app.delete('/deleteOrder/:id',verifyToken, async (req,res)=>{
+            const id = req.params.id;
+            if(req?.decodedEmail){
+            const query = {_id: ObjectId(id)};
+            const result = await myCollections.deleteOne(query);
+            res.json(result);
+            }else{
+                res.json({})
+            }
+        })
+
+
+         // updating-the-product-status-and-verify-user-with-jwt
+         app.put('/setStatus/:id',verifyToken, async (req,res)=>{
+            if(req?.decodedEmail){
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const upsert= {upsert: true};
+                const updateDocs ={$set:{status: 'shipped'}};
+                const result = await myCollections.updateOne(query,updateDocs, upsert);
+                res.json(result); 
+            }else{
+                res.json({})
+            }
+        })
+
+        // get-the-all-products-of-logged-user
+        app.get('/singleUserOrders/:email', async (req,res)=>{
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await myCollections.find(query).toArray();
+            res.json(result)
+        })
+
+
+
+       // make-admin-with-email-and-verify-user-with-jwt 
+        app.put('/makeAdmin/:email',verifyToken, async (req,res)=>{
+            const email = req.params.email;
+           if(req?.decodedEmail){
+               const query = {email: email};
+               const upsert = { upsert: true };
+               const updateDocs = {$set:{role: 'admin'}};
+               const result = await userCollections.updateOne(query,updateDocs,upsert);
+               res.send(result)
+           }else{
+            res.json({})
+           }
+        })
+
+
+
+        // get-all-main-products
+        app.get('/getAllProducts', async (req,res)=>{
+            const query = {};
+            const result = await shopCollections.find(query).toArray();
+            res.json(result);
+        })
+
+
+         // get-product-for-payment
+         app.get('/paymentProduct/:id', async (req,res)=>{
+             const id = req.params.id;
+             const query = {_id: ObjectId(id)};
+             const result = await myCollections.findOne(query);
+             res.json(result);
+        })
+
+
+
+        // delete-the-main-product-and-verify-user-with-jwt
+        app.delete('/deleteProduct/:id',verifyToken, async (req,res)=>{
+            if(req?.decodedEmail){
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const result = await shopCollections.deleteOne(query);
+                res.json(result);
+            }else{
+                res.json({})
+            }
+        })
+
+
+
+        // add-a-product-to-main-product-and-verify-user-with-jwt
+        app.post('/addProduct',verifyToken, async (req,res)=>{
+            if(req?.decodedEmail){
+                const product = req.body;
+                const result = await shopCollections.insertOne(product);
+                res.json(result);
+            }else{
+                res.json({})
+            }
+        })
+
+
+        app.put('/updateStock',verifyToken, async (req,res)=>{
+            if(req?.decodedEmail){
+                const stock = req.body.TotalStock.stock;
+                console.log(stock+'from stock')
+                const id = req.body.TotalStock.id;
+                const options = { upsert: true };
+                const query = {_id: ObjectId(id)}
+                const updateDoc = {
+                  $set: {
+                    stock: stock,
+                  },
+                };
+                
+                const result = await shopCollections.updateOne(query, updateDoc, options);
+                res.json(result);
+            }else{
+                res.json({})
+            }
+        })
+
+
+
+
+
+        app.put('/updateStockWithDes',verifyToken, async (req,res)=>{
+            if(req?.decodedEmail){
+                const stock = req.body.stock.stock;
+                const description = req.body.stock.description;
+                const options = { upsert: true };
+                const query = {description: description};
+                const updateDoc = {
+                  $set: {
+                    stock: stock,
+                  },
+                };
+               
+                const result = await shopCollections.updateOne(query, updateDoc, options);
+                res.json(result);
+            }else{
+                res.json({})
+            }
+        })
+
+
+
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = Number(paymentInfo.price) * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                payment_method_types: ['card']
+            });
+            res.json({ clientSecret: paymentIntent.client_secret })
+        })
+
+
+
+
+        app.put('/paidProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            };
+            const result = await myCollections.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log('server listening on port ' + port);
+});
---
+++ products.js
@@ -0,0 +1,322 @@
+[{
+        "name": "courlogy",
+        "description": "Curology is a skin care company that provides personalized remote skin care treatment and sells customized skin care products online.",
+        "rate": "5",
+        "img": "https://i.ibb.co/NC8Sx49/curology-w-K0h-mlvfuc-unsplash-removebg-preview.png",
+        "price": "15",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "lit mallow",
+        "description": "We’re working hard to get all your orders baked and packed up so forgive us if we’re a little slow on SM & DMs at this time. We’re working on the good stuff ",
+        "rate": "4",
+        "img": "https://i.ibb.co/gPShkgd/download-removebg-preview.png",
+        "price": "8",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Fire and lovely",
+        "description": "Fair & Lovely Advanced Multivitamin Face Cream, For Daily Use, Fades Dark Spots, Lighten Skin For Glowing, Matte Finish, 110 g",
+        "rate": "5",
+        "img": "https://i.ibb.co/JsMjFtB/firence-removebg-preview.png",
+        "price": "3",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Himalya cream",
+        "description": "BoldPlex 3 Bond Repair Hair Protein Treatment Mask for Dry Damaged hair - Hydrating & Conditioning for Curly, Coloured, Frizzy, Broken or Bleached",
+        "rate": "4.8",
+        "img": "https://i.ibb.co/6H98S1F/imagddes-removebg-preview.png",
+        "price": "4",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Nail polish",
+        "description": "Nail Polish for Women Black Color Nail Polish -50",
+        "rate": "3.9",
+        "img": "https://i.ibb.co/brDK5sb/imagddfes-removebg-preview.png",
+        "price": "2",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Sunscreen cream",
+        "description": "VLCC Sun Screen Lotion With Lemon SPF 25 PA+ 150ML INDIAN",
+        "rate": "5",
+        "img": "https://i.ibb.co/qmkfvKT/images-removebg-preview.png",
+        "price": "6",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Makeup box",
+        "description": "High grade clear acrylic material, four drawers and lipstick rack holder, easy to organize, keep your makeup neat and tidy.",
+        "rate": "4",
+        "img": "https://i.ibb.co/hmJ5kQx/lipstik-removebg-preview.png",
+        "price": "15",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Makeup box",
+        "description": "The 5 drawer cosmetics and jewelry storage box is suitable for holding and displaying cosmetics such as lipstick, jewelry and other accessories.",
+        "rate": "4.5",
+        "img": "https://i.ibb.co/TLjyky1/makeup-removebg-preview.png",
+        "price": "20",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Shampoo",
+        "description": "Argan Oil Shampoo Is Formulated Using Oil From Fruit Of Argan Tree Of Morocco. Enrich With Vitamin E, A Natural Anti Oxidants To Nourish And Rejuvenate Dry And Damaged Hair",
+        "rate": "4.9",
+        "img": "https://i.ibb.co/PgDxDXr/shampo-removebg-preview.png",
+        "price": "9",
+        "category": "beautyProduct"
+    },
+    {
+        "name": "Sunscreen lotion",
+        "description": "100% Original Products You can return the product within 15 days if the delivered product is damaged, expired, different from ordered.",
+        "rate": "5",
+        "img": "https://i.ibb.co/mRmZRYS/sunscreen-lotion-removebg-preview.png",
+        "price": "11",
+        "category": "beautyProduct"
+    },
+
+    {
+        "name": "Cap",
+        "description": "Summer Baseball Caps for Men Women Outdoor Sport Sun Hat Running Visor Cap Casual Hats Mesh Caps",
+        "rate": "3",
+        "img": "https://i.ibb.co/DYkFpxJ/cap-removebg-preview.png",
+        "price": "3",
+        "category": "cloths"
+    },
+    {
+        "name": "Huddi",
+        "description": "LINGKING PARK Printed Cotton & Fleece Febrics Hoodie For Men",
+        "rate": "4.8",
+        "img": "https://i.ibb.co/w7DGdR0/huddi-removebg-preview.png",
+        "price": "200",
+        "category": "cloths"
+    },
+    {
+        "name": "Jense pant",
+        "description": "Latest Design High Quality Distressed Ripped Washed Loose Vintage Jeans",
+        "rate": "4.5",
+        "img": "https://i.ibb.co/w7DGdR0/huddi-removebg-preview.png",
+        "price": "250",
+        "category": "cloths"
+    },
+    {
+        "name": "Scraf",
+        "description": "Male Casual Wear Blue Woolen Scarves",
+        "rate": "4.1",
+        "img": "https://i.ibb.co/jhvvfXJ/scraf-removebg-preview.png",
+        "price": "30",
+        "category": "cloths"
+    },
+    {
+        "name": "Shirt",
+        "description": "Flaunt the modern-day classics with intrinsically American Levi’s clothing. AJIO brings the ever-stylish 511 Levi’s jeans, rugged denim trucker jackets skinny",
+        "rate": "4.3",
+        "img": "https://i.ibb.co/jhvvfXJ/scraf-removebg-preview.png",
+        "price": "280",
+        "category": "cloths"
+    },
+    {
+        "name": "Short frok",
+        "description": "YENKYE Newest Women Contrast Color Floral Print Boho Party Dresses Sexy V Neck Sashes Robe Ladies Draped Layered Short Dress",
+        "rate": "3",
+        "img": "https://i.ibb.co/GHwM3BP/skart-removebg-preview.png",
+        "price": "250",
+        "category": "cloths"
+    },
+    {
+        "name": "Long frok",
+        "description": "H Han Queen Womens New Polka Dot Puff Sleeve Elegant Dress Office Lady Evening Party Bodycon Sheath Pencil Dresses Work Vestidos",
+        "rate": "5",
+        "img": "https://i.ibb.co/QdRNp6N/skrt-removebg-preview.png",
+        "price": "450",
+        "category": "cloths"
+    },
+    {
+        "name": "T-Shirt",
+        "description": "Stylist Cotton Men Clothing Casual Tops T-Shirts",
+        "rate": "5",
+        "img": "https://i.ibb.co/r6NgH1v/T-shirt-removebg-preview.png",
+        "price": "170",
+        "category": "cloths"
+    },
+    {
+        "name": "Underware",
+        "description": "Hanes Men's 7 Pack Ultimate Full-Cut Pre Shrunk Brief",
+        "rate": "4.4",
+        "img": "https://i.ibb.co/3TGSHJg/underware-removebg-preview.png",
+        "price": "80",
+        "category": "cloths"
+    },
+    {
+        "name": "Winter court",
+        "description": "Solid Navy Blue colour stylish Jacket For Men",
+        "rate": "4",
+        "img": "https://i.ibb.co/FHSHskK/winter-Court-removebg-preview.png",
+        "price": "400",
+        "category": "cloths"
+    },
+    {
+        "name": "Almare",
+        "description": "Regal Household Almirah Popular Choco Blazing Star",
+        "rate": "5",
+        "img": "https://i.ibb.co/G9FK0Xt/almare-removebg-preview.png",
+        "price": "700",
+        "category": "furniture"
+    },
+    {
+        "name": "Bed",
+        "description": "Regal Valentina Wooden King Bed",
+        "rate": "4.2",
+        "img": "https://i.ibb.co/qrgrH3f/bed-removebg-preview.png",
+        "price": "2000",
+        "category": "furniture"
+    },
+    {
+        "name": "Book case",
+        "description": "Melamine Laminated Board Book Shelf 15 feet by 10 feet",
+        "rate": "4",
+        "img": "https://i.ibb.co/TrvLf7M/bookcase-removebg-preview.png",
+        "price": "350",
+        "category": "furniture"
+    },
+    {
+        "name": "Chair",
+        "description": "Egg Sitter Seat Cushion with Non-Slip Cover, Breathable Honeycomb Design Absorbs Pressure Points",
+        "rate": "5",
+        "img": "https://i.ibb.co/Z1bY2LS/chairetwo-removebg-preview.png",
+        "price": "220",
+        "category": "furniture"
+    },
+    {
+        "name": "Gaming Chair",
+        "description": "Fantech Alpha GC-185X Gaming Chair · EVOLUR LD001 Gaming Chair Orange · MeeTion MT-CHR05 Cheap Mesh Professional E-Sport Office Gaming Chair",
+        "rate": "4.3",
+        "img": "https://i.ibb.co/tMwyt8t/chair-removebg-preview.png",
+        "price": "950",
+        "category": "furniture"
+    },
+    {
+        "name": "Wall clock",
+        "description": "Diling Brand White Dial with Pink Color Border Medium Wall Clock",
+        "rate": "3.4",
+        "img": "https://i.ibb.co/2jpdwyg/clock-removebg-preview.png",
+        "price": "420",
+        "category": "furniture"
+    },
+    {
+        "name": "Desk",
+        "description": "Flexispot EG1 Electric Standing Desk, Height Adjustable Desk Sit Stand Up Desk with Splice Board Home Office Desks 55 x 28 Inches Vici (White Frame + Maple Top)",
+        "rate": "4",
+        "img": "https://i.ibb.co/w0b3YnS/desk-removebg-preview.png",
+        "price": "580",
+        "category": "furniture"
+    },
+    {
+        "name": "Get leg table",
+        "description": "28 in Metal Table Legs Industrial Desk Legs, Heavy Duty Furiniture Legs, DIY Legs, Coffee Table Legs, Dining Table",
+        "rate": "4.1",
+        "img": "https://i.ibb.co/P9H3bpM/gateleg-Table-removebg-preview.png",
+        "price": "740",
+        "category": "furniture"
+    },
+    {
+        "name": "Book self",
+        "description": "Sauder Beginnings 5-Shelf Bookcase, Highland Oak finish",
+        "rate": "4",
+        "img": "https://i.ibb.co/Vv0s0y9/sresser-removebg-preview.png",
+        "price": "620",
+        "category": "furniture"
+    },
+    {
+        "name": "Whatnot",
+        "description": "Simple Whatnots: A Batch of Satisfyingly Scrappy Little Quilts",
+        "rate": "2.3",
+        "img": "https://i.ibb.co/mH1fSSg/whatnot-removebg-preview.png",
+        "price": "480",
+        "category": "furniture"
+    },
+    {
+        "name": "Boot",
+        "description": "Timberland PRO Men's Pit Boss 6 Inch Steel Safety Toe Industrial Work Boot",
+        "rate": "4.2",
+        "img": "https://i.ibb.co/8n3kpLC/boots-removebg-preview.png",
+        "price": "880",
+        "category": "shoes"
+    },
+    {
+        "name": "Cros",
+        "description": "Crocs Men's and Women's Bistro Pro Literide Clog | Slip Resistant Work Shoes",
+        "rate": "3.4",
+        "img": "https://i.ibb.co/GnS44zq/crocs-removebg-preview.png",
+        "price": "520",
+        "category": "shoes"
+    },
+    {
+        "name": "Earth origin shoe",
+        "description": "Earth Origins Mavis Women's Slip On",
+        "rate": "5",
+        "img": "https://i.ibb.co/qm5Zp2k/earth-origin-shoe-removebg-preview.png",
+        "price": "1020",
+        "category": "shoes"
+    },
+    {
+        "name": "Flip flops",
+        "description": "Quiksilver Men's Molokai 3 Point Flip Flop Sandals",
+        "rate": "3.4",
+        "img": "https://i.ibb.co/m4N0WQR/flip-Flops-removebg-preview.png",
+        "price": "70",
+        "category": "shoes"
+    },
+    {
+        "name": "Formal shoe",
+        "description": "Jousen Men's Dress Shoes Leather Classic Formal Mens Oxfords Retro Derby Oxford",
+        "rate": "5",
+        "img": "https://i.ibb.co/QHcRLQB/formal-Shoe-removebg-preview.png",
+        "price": "1270",
+        "category": "shoes"
+    },
+    {
+        "name": "High hill",
+        "description": "Naturalizer Women's Vera Heeled Sandal, Barely Nude, 7 Wide Naturalizer Women 's Vera Heeled Sandal, Barely Nude, 7 Wide",
+        "rate": "4.1",
+        "img": "https://i.ibb.co/BTTcXfF/high-hill-removebg-preview.png",
+        "price": "870",
+        "category": "shoes"
+    },
+    {
+        "name": "High hill",
+        "description": "LifeStride Women's Giovanna 2 Pump",
+        "rate": "4.5",
+        "img": "https://i.ibb.co/fQ1F61y/hill-removebg-preview.png",
+        "price": "920",
+        "category": "shoes"
+    },
+    {
+        "name": "Leather",
+        "description": "Very Fine Men's Valerian Soft Supple Lambskin Leather Latin Salsa Tango Samba Dance Shoe",
+        "rate": "3",
+        "img": "https://i.ibb.co/9Nz5YXh/leather-removebg-preview.png",
+        "price": "1020",
+        "category": "shoes"
+    },
+    {
+        "name": "Sandal",
+        "description": "Men Buckle X Grey Sports Sandal",
+        "rate": "5",
+        "img": "https://i.ibb.co/Xj53x0r/sandle-removebg-preview.png",
+        "price": "120",
+        "category": "shoes"
+    },
+    {
+        "name": "Sneaker",
+        "description": "Fashion Leather Slip On Men Driving Moccasins Loafers Casual Shoes",
+        "rate": "4",
+        "img": "https://i.ibb.co/XWpKP51/seankers-removebg-preview.png",
+        "price": "920",
+        "category": "shoes"
+    }
+]
